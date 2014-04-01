359a8e1c778bed19e16345c6ec0bfa01670f464f
Alvaro Millalen <alvaro.millalen@khipu.com>
2014-04-01 11:11:40 -0300

    add pdf417 barcode to generated pdf

--- khpdf-ejb/src/main/java/com/khipu/khpdf/PdfGeneratorImpl.java
+++ khpdf-ejb/src/main/java/com/khipu/khpdf/PdfGeneratorImpl.java
@@ -1,7 +1,9 @@
 package com.khipu.khpdf;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.BarcodePDF417;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 import com.itextpdf.tool.xml.exceptions.RuntimeWorkerException;
@@ -25,6 +27,52 @@ public class PdfGeneratorImpl implements PdfGenerator, PdfGeneratorRemote {
     @PostConstruct
     public void postConstruct() {
         xmlWorkerHelper = XMLWorkerHelper.getInstance();
+    }
+
+    @Override
+    public byte[] pdfFromHtmlAndPdf417(byte[] html, Map<PdfFields,String> fields, PDF417Data pdf417data) throws DocumentGenerationException {
+        ByteArrayInputStream in = new ByteArrayInputStream(pdfFromHtml(html,fields));
+        try {
+            BarcodePDF417 pdf417 = new BarcodePDF417();
+            pdf417.setCodeRows(pdf417data.getCodeRows());
+            pdf417.setCodeColumns(pdf417data.getCodeCols());
+            pdf417.setErrorLevel(pdf417data.getErrorLevel());
+            pdf417.setLenCodewords(pdf417data.getCodewordsLen());
+            pdf417.setAspectRatio(pdf417data.getAspectRadio());
+            pdf417.setOptions(BarcodePDF417.PDF417_FORCE_BINARY);
+            pdf417.setText(pdf417data.getData());
+
+            Image pdf417Image = Image.getInstance(pdf417.getImage());
+
+            byte[] white= {(byte)255};
+            Image quietZoneImg = Image.getInstance(1, 1, 1, 8, white);
+            quietZoneImg.scaleAbsolute(pdf417Image.getWidth() + pdf417data.getQuietZoneWidth() * 2, pdf417Image.getHeight());
+
+            quietZoneImg.setAbsolutePosition(pdf417data.getAbsoluteXPosition(),
+                    pdf417data.getAbsoluteYPosition());
+
+
+            pdf417Image.setAbsolutePosition(pdf417data.getAbsoluteXPosition() + pdf417data.getQuietZoneWidth(),
+                    pdf417data.getAbsoluteYPosition());
+
+
+            pdf417Image.setIndentationLeft(pdf417data.getQuietZoneWidth());
+            pdf417Image.setIndentationRight(pdf417data.getQuietZoneWidth());
+
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            PdfReader reader = new PdfReader(in);
+            PdfStamper stamper = new PdfStamper(reader, out);
+            stamper.getOverContent(1).addImage(quietZoneImg);
+            stamper.getOverContent(1).addImage(pdf417Image);
+
+            stamper.close();
+            return out.toByteArray();
+
+        } catch (IOException|DocumentException e) {
+            throw new DocumentGenerationException(e);
+        }
+
     }
 
     @Override
